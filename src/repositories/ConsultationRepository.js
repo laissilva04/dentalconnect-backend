14290dd41bb117483d84e20f6ef0fb6afeab5e53
Lais Barbosa <laissilva@edu.unifor.br>
2025-04-22 10:23:06 -0300

    Implementando CRUD da tabela consultas

---
+++ src/repositories/ConsultationRepository.js
@@ -0,0 +1,89 @@
+const { createClient } = require('@supabase/supabase-js');
+
+const supabase = createClient(
+    process.env.SUPABASE_URL,
+    process.env.SUPABASE_KEY
+);
+
+class ConsultationRepository {
+
+    async getAllConsultation() {
+        const { data, error } = await supabase.from('consulta').select('*');
+        if (error) throw new Error(error.message);
+        return data;
+    }
+
+    /**
+     * Cria uma nova consulta no banco de dados
+     * @param {Object} consultation 
+     */
+    async createConsultation(consultation) {
+        const { data, error } = await supabase.from('consulta').insert([consultation]);
+        if (error) {
+            console.error('Erro ao criar consulta:', error.message);
+            throw new Error(error.message);
+        }
+        return data;
+    }
+
+    /**
+     * Busca uma consulta pelo ID.
+     * @param {number} id
+     * @returns {Object|null}
+     */
+    async findById(id) {
+        const { data, error } = await supabase
+        .from('consulta')
+        .select('*')
+        .eq('id', id)
+        .single(); // Retorna um único registro
+
+        if (error) {
+        if (error.code === 'PGRST116') return null; // Caso o local não seja encontrado
+        console.error('Erro ao buscar consulta por ID:', error.message);
+        throw new Error(error.message);
+        }
+
+        return data;
+    }
+
+    /**
+     * Atualiza uma consulta
+     * @param {number} id 
+     * @param {Object} updates 
+     */
+    async updateConsultation(id, updates) {
+        const { data, error } = await supabase
+            .from('consulta')
+            .update(updates)
+            .match({ id })
+            .single();
+
+        if (error) {
+            console.error('Erro ao atualizar consulta:', error.message);
+            throw new Error(error.message);
+        }
+
+        return data;
+    }
+
+    /**
+     * Remove uma consulta
+     * @param {number} id 
+     */
+    async deleteConsultation(id) {
+        const { data, error } = await supabase
+            .from('consulta')
+            .delete()
+            .match({ id });
+
+        if (error) {
+            console.error('Erro ao deletar consulta:', error.message);
+            throw new Error(error.message);
+        }
+
+        return data;
+    }
+};
+
+module.exports = ConsultationRepository;
---
+++ src/services/ConsultationService.js
@@ -0,0 +1,78 @@
+const ConsultationRepository = require('../repositories/ConsultationRepository')
+
+
+class ConsultationService {
+  constructor() {
+    this.consultationRepository = new ConsultationRepository();
+  }
+
+  /**
+   * Retorna todos as consultas cadastrados.
+   * @returns {Array<Consultation>}
+   */
+  async listConsultation() {
+    return await this.consultationRepository.getAllConsultation();
+  }
+
+  /**
+   * Cria uma nova consulta.
+   * @param {Object} consultation
+   */
+  async createConsultation(consultation) {
+    const { data, horario, paciente, dentista, local, status } = consultation;
+
+    // Validação dos dados
+    if (!data || !horario || !paciente || !dentista || !local || !status) {
+      throw new Error('Todos os campos são obrigatórios.');
+    }
+
+    // Envia os dados validados para o repositório
+    return await this.consultationRepository.createConsultation({ data, horario, paciente, dentista, local, status });
+  }
+
+  /**
+   * Atualiza uma consulta existente.
+   * @param {number} id
+   * @param {Object} updates
+   */
+  async updateConsultation(id, updates) {
+    if (!id) {
+      throw new Error('O ID da consulta é obrigatório.');
+    }
+
+    // Validação dos dados
+    const { data, horario, paciente, dentista, local, status } = updates;
+    if (!data || !horario || !paciente || !dentista || !local || !status) {
+      throw new Error('Pelo menos um campo deve ser fornecido para atualização.');
+    }
+
+    return await this.consultationRepository.updateConsultation(id, updates);
+  }
+
+  /**
+   * Deleta uma consulta.
+   * @param {number} id
+   */
+  async deleteConsultation(id) {
+    if (!id) {
+      throw new Error('O ID da consulta é obrigatório.');
+    }
+
+    return await this.consultationRepository.deleteConsultation(id);
+  }
+
+   /**
+   * Busca uma consulta pelo ID.
+   * @param {number} id
+   * @returns {Object|null}
+   */
+   async findConsultaById(id) {
+    if (!id) {
+      throw new Error('O ID da consulta é obrigatório.');
+    }
+
+    return await this.consultationRepository.findById(id);
+  }
+}
+
+module.exports = ConsultationService;
