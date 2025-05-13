92d8ea56db2a06a2ff178986ed6873fead89fef9
astvri <pehgyt@gmail.com>
2025-05-13 18:28:22 -0300

    CRUD DentistaServicos

--- src/entities/DentistEntity.js
+++ src/entities/DentistEntity.js
@@ -2,7 +2,7 @@ class Dentist {
     constructor(id, created_at, id_usuario, numero_cro) {
         this.id = id;
         this.created_at = created_at;
-        this.usuario_id = usuario_id;
+        this.id_usuario = id_usuario;
         this.numero_cro = numero_cro;
         this.usuario = usuario; 
     }
